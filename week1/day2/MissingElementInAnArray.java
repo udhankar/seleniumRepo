436049ea2ea5db4ee87e6dcb4254e998e3bc2ab6
udhankar <140312217+udhankar@users.noreply.github.com>
2023-07-28 11:29:55 +0530

    Update MissingElementInAnArray.java

--- week1/day2/MissingElementInAnArray.java
+++ week1/day2/MissingElementInAnArray.java
@@ -7,11 +7,12 @@ public class MissingElementInAnArray {
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 3, 4, 7, 6, 8 };
 		Arrays.sort(arr);
-		for (int i = arr[0]; i < arr.length; i++)
-			if (i != arr[i]) {
-				System.out.println(arr[i]);
+		for (int i = 0; i < arr.length - 1; i++)
+			if (arr[i + 1] != arr[i] + 1) {
+				System.out.println(arr[i] + 1);
 				break;
 			}
 	}
 
 }
+
